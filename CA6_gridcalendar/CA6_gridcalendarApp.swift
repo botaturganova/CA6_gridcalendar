0a7e79ada79dd337e651e273a2e83ba8faabfcd7
Akbota Turganova <botaturganova@gmail.com>
2024-12-18 14:38:50 +0500

    initial

---
+++ CA6_gridcalendar/CA6_gridcalendarApp.swift
@@ -0,0 +1,17 @@
+//
+//  CA6_gridcalendarApp.swift
+//  CA6_gridcalendar
+//
+//  Created by bota on 18.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct CA6_gridcalendarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
